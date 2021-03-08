e9a757e3775a8bcd1ed9cdac1a3ca9664987fa92
Bruno Monteiro <brunotaxi78@outlook.pt>
2021-03-08 10:16:45 +0000

    Changes to React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,38 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Scroll from '../components/Scroll';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App() {
 
-    componentDidMount() {
+    const [robots, setRobots] = useState([])
+    const [searchfield, setSearchfield] = useState('')
+
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({robots : users}));
-        
+        .then(users => setRobots(users));
+    },[])
+
+    const OnSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    OnSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-    }
-
-    render() {
-        const filter = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const filter = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
     return (
         <div className='tc'>
             <h1 className="f-6">Master Players</h1>
-            <SearchBox searchChange={this.OnSearchChange}/>
+            <SearchBox searchChange={OnSearchChange}/>
             <Scroll>
                 <ErrorBoundry>
                     <CardList robots={filter}/>
@@ -40,7 +34,7 @@ class App extends React.Component {
             </Scroll>
         </div>
     )
-    }
+    
 }
 
 export default App;
